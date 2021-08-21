056147ffc366296e1dc8940f855857391c4a4757
dionsongus <35827135+dionsongus@users.noreply.github.com>
2021-08-21 13:11:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,11 +19,12 @@ app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
-
+print("SQLALCHEMY_DATABASE_URI", app.config['SQLALCHEMY_DATABASE_URI'])
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+print('db ', db)
 
 Pet = create_classes(db)
 
